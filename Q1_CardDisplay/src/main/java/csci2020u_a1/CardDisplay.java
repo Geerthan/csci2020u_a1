b7d2841871bf80f38c31a8adbb34630dc517336b
Geerthan Srikantharajah <geerthan.srikantharajah@gmail.com>
2019-03-04 23:30:59 -0500

    Created Q1 application framework

---
+++ Q1_CardDisplay/src/main/java/csci2020u_a1/CardDisplay.java
@@ -0,0 +1,18 @@
+
+package csci2020u_a1.Q1_CardDisplay;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CardDisplay extends Application {
+	
+	public void start(Stage primaryStage) {
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
