286cdf9cd7bd487dfe23b6145bc71c7c0f627145
Geerthan Srikantharajah <geerthan.srikantharajah@gmail.com>
2019-03-04 23:43:29 -0500

    Created Q4 application framework

---
+++ Q4_Histogram/src/main/java/csci2020u_a1/Histogram.java
@@ -0,0 +1,18 @@
+
+package csci2020u_a1.Q4_Histogram;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Histogram extends Application {
+	
+	public void start(Stage primaryStage) {
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
