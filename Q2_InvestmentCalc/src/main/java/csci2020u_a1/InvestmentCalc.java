6b5aebfaa548ebe4139021858b9ae94503bd4856
Geerthan Srikantharajah <geerthan.srikantharajah@gmail.com>
2019-03-04 23:36:57 -0500

    Created Q2 application framework

---
+++ Q2_InvestmentCalc/src/main/java/csci2020u_a1/InvestmentCalc.java
@@ -0,0 +1,18 @@
+
+package csci2020u_a1.Q2_InvestmentCalc;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class InvestmentCalc extends Application {
+	
+	public void start(Stage primaryStage) {
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
