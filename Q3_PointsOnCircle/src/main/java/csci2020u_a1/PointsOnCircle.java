640c295d9d91399ca3bddf4daf1aa1508f022445
Geerthan Srikantharajah <geerthan.srikantharajah@gmail.com>
2019-03-04 23:40:06 -0500

    Created Q3 application framework

---
+++ Q3_PointsOnCircle/src/main/java/csci2020u_a1/PointsOnCircle.java
@@ -0,0 +1,18 @@
+
+package csci2020u_a1.Q3_PointsOnCircle;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PointsOnCircle extends Application {
+	
+	public void start(Stage primaryStage) {
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
