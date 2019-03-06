244d5cc3a37c5b14dee3c10c4da798225691d5c2
Geerthan Srikantharajah <geerthan.srikantharajah@gmail.com>
2019-03-06 18:38:14 -0500

    Debugged Q1

--- Q1_CardDisplay/src/main/java/csci2020u/AssignmentQ1/Main.java
+++ Q1_CardDisplay/src/main/java/csci2020u/AssignmentQ1/Main.java
@@ -1,42 +1,33 @@
 package csci2020.AssignmentQ1;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Arc;
-import javafx.scene.shape.ArcType;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.util.Random;
 
 
 public class Main extends Application {
 
-  @Override
-  public void start(Stage primaryStage) throws Exception
-  {
+  public void start(Stage primaryStage) {
       primaryStage.setTitle("Question_1");
 
-      Pane pane = new Pane();
       HBox hbox = new HBox(3);
       Random rand = new Random();
-      int n1 = rand.nextInt(53) + 1;
-      int n2 = rand.nextInt(53) + 1;
-      int n3 = rand.nextInt(53) + 1;
+      int n1 = rand.nextInt(54) + 1;
+      int n2 = rand.nextInt(54) + 1;
+      int n3 = rand.nextInt(54) + 1;
       while (true)
       {
         if (n2 == n1)
         {
-          int n2 = rand.nextInt(53) + 1;
+          n2 = rand.nextInt(53) + 1;
         }
         else if (n3 == n1 || n3 == n2)
         {
-          int n3 = rand.nextInt(53) + 1;
+          n3 = rand.nextInt(53) + 1;
         }
         else
         {
@@ -44,21 +35,21 @@ public class Main extends Application {
         }
       }
 
-      Image card1 = new Image("Cards/" + n1 + ".png");
-      Image card2 = new Image("Cards/" + n2 + ".png");
-      Image card3 = new Image("Cards/" + n3 + ".png");
+      Image card1 = new Image("file:Cards/" + n1 + ".png");
+      Image card2 = new Image("file:Cards/" + n2 + ".png");
+      Image card3 = new Image("file:Cards/" + n3 + ".png");
 
       hbox.getChildren().add(new ImageView(card1));
       hbox.getChildren().add(new ImageView(card2));
       hbox.getChildren().add(new ImageView(card3));
 
-
-      Scene scene = new Scene(pane, 400, 600);
+      Scene scene = new Scene(hbox);
+      
       primaryStage.setScene(scene);
       primaryStage.show();
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  }
+  public static void main(String[] args) {
+     launch(args);
+  }
 }
