c5065fefebbee8caa6c58b6e4d4a3764aecf28ff
Geerthan Srikantharajah <geerthan.srikantharajah@gmail.com>
2019-03-06 12:24:57 -0500

    Debugged Q2

--- Q2_InvestmentCalc/src/main/java/csci2020u/AssignmentQ2/Main.java
+++ Q2_InvestmentCalc/src/main/java/csci2020u/AssignmentQ2/Main.java
@@ -14,53 +14,65 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 public class Main extends Application {
 
-
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
-        primaryStage.setTitle("Question_3");
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Question_2");
 
         GridPane pane = new GridPane();
+        pane.setPadding(new Insets(10, 10, 10, 10));
         TextField invest, years, rate, result;
 
-        pane.getChildren().add(new Label("Investment Amount: "), 0, 0);
+        pane.add(new Label("Investment Amount: "), 0, 0);
         invest = new TextField();
         invest.setPromptText("Amount");
-        pane.getChildren().add(invest, 0, 1);
+        pane.add(invest, 1, 0);
 
-        pane.getChildren().add(new Label("Years: "), 1, 0);
+        pane.add(new Label("Years: "), 0, 1);
         years = new TextField();
         years.setPromptText("Years");
-        pane.getChildren().add(years, 1, 1);
+        pane.add(years, 1, 1);
 
-        pane.getChildren().add(new Label("Annual Interest Rate: "), 2, 0);
+        pane.add(new Label("Annual Interest Rate: "), 0, 2);
         rate = new TextField();
         rate.setPromptText("Rate");
-        pane.getChildren().add(rate, 2, 1);
+        pane.add(rate, 1, 2);
         
-        pane.getChildren().add(new Label("Future Value: "), 3, 0);
+        pane.add(new Label("Future Value: "), 0, 3);
         result = new TextField();
-        pane.getChildren().add(result, 3, 1);
+        pane.add(result, 1, 3);
         Button calculate = new Button("Calculate");
 
-        calculate.setOnAction(new EventHandler<ActionEvent>()) {
-          @Override
-          public void handle(ActionEvent event)
-          {
-            double inv = double(invest.getText());
-            double year = double(years.getText());
-            double r = double(rate.getText());
-            double future;
-            future = inv * Math.pow((1 + r), year*12);
+        calculate.setOnAction(new EventHandler<ActionEvent>() {
+            
+          public void handle(ActionEvent event) {
+            double inv = Double.valueOf(invest.getText());
+            double year = Double.valueOf(years.getText());
+            double r = Double.valueOf(rate.getText()) / 100 / 12;
+            String future;
+            future = String.valueOf(inv * Math.pow((1 + r), year*12));
             result.setText(future);
           }
-        }
+          
+        });
 
-        pane.getChildren().add(calculate, 4, 1);
+        pane.add(calculate, 1, 4);
+        GridPane.setHalignment(calculate, HPos.RIGHT);
 
-        Scene scene = new Scene(pane, 600, 600);
+        Scene scene = new Scene(pane);
         primaryStage.setScene(scene);
         primaryStage.show();
 
